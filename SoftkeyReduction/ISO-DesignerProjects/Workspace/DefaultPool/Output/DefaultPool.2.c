18d617f39173d7eb885a6a435ebd7bf47d4f864b
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2023-01-20 17:44:11 +0100

    12 key Built.

---
+++ SoftkeyReduction/ISO-DesignerProjects/Workspace/DefaultPool/Output/DefaultPool.2.c
@@ -0,0 +1,35 @@
+// ISO-Designer ISO 11783   Version 5.6.1.5243 Jetter AG
+// Do not change!
+
+#include "DefaultPool.c.h"
+
+const unsigned long ISO_OP_MEMORY_CLASS isoOP_DefaultPool_Offset[] = {
+	     1,  // WorkingSet_0_Offset
+	    21,  // DataMask_1000_Offset
+	    29,  // SoftKeyMask_4000_Offset
+	    59,  // SoftKey_5000_Offset
+	    72,  // SoftKey_5001_Offset
+	    85,  // SoftKey_5002_Offset
+	    98,  // SoftKey_5003_Offset
+	   111,  // SoftKey_5004_Offset
+	   124,  // SoftKey_5005_Offset
+	   137,  // SoftKey_5006_Offset
+	   150,  // SoftKey_5007_Offset
+	   163,  // SoftKey_5008_Offset
+	   176,  // SoftKey_5009_Offset
+	   189,  // SoftKey_5010_Offset
+	   202,  // SoftKey_5011_Offset
+	   215,  // OutputNumber_12000_Offset
+	   244,  // OutputNumber_12001_Offset
+	   273,  // OutputNumber_12002_Offset
+	   302,  // OutputNumber_12003_Offset
+	   331,  // OutputNumber_12004_Offset
+	   360,  // OutputNumber_12005_Offset
+	   389,  // OutputNumber_12006_Offset
+	   418,  // OutputNumber_12007_Offset
+	   447,  // OutputNumber_12008_Offset
+	   476,  // OutputNumber_12009_Offset
+	   505,  // OutputNumber_12010_Offset
+	   534,  // OutputNumber_12011_Offset
+	   563,  // FontAttributes_23000_Offset
+};  // isoOP_DefaultPool_Offset
